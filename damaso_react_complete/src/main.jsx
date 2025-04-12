2ebd1ab8363e3bc35a99918b363f639779a0eabd
Damaso2411 <Damaso2411@gmail.com>
2025-04-12 14:01:00 -0400

    Add files via upload

---
+++ damaso_react_complete/src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import DamasoWeb from './DamasoWeb';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DamasoWeb />
+  </React.StrictMode>
+);
